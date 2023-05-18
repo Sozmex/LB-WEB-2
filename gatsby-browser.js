6d3229f9e4593c5277d19abf29fc1a4557470c9a
Sozmex <zachmex1@gmail.com>
2023-05-18 16:25:35 -0400

    trying to get styles ot apply

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,13 +1,13 @@
 // custom typefaces
-import "@fontsource/montserrat/variable.css"
-import "@fontsource/merriweather"
+//import "@fontsource/montserrat/variable.css"
+//import "@fontsource/merriweather"
 // normalize CSS across browsers
 //import "./src/normalize.css"
 // custom CSS styles
 //import "./src/style.css"
 
 //import tailwind
-import "./src/global.css";
+import "./src/styles/global.css";
 
 // Highlighting for code blocks
-import "prismjs/themes/prism.css"
+//import "prismjs/themes/prism.css"
