7a5b85297ea8d56892fa72288a0d7625018209c3
Sozmex <zachmex1@gmail.com>
2023-05-18 16:12:22 -0400

    fixed comments for below commit

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,6 +2,11 @@
 import "@fontsource/montserrat/variable.css"
 import "@fontsource/merriweather"
 // normalize CSS across browsers
+//import "./src/normalize.css"
+// custom CSS styles
+//import "./src/style.css"
+
+//import tailwind
 import "./src/styles/global.css";
 
 // Highlighting for code blocks
