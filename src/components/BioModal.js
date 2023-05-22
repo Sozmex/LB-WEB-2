afe94b0ceade9adebb0c9eea161e4ef22a98a7ed
Sozmex <zachmex1@gmail.com>
2023-05-22 13:50:01 -0400

    updated bios

--- src/components/BioModal.js
+++ src/components/BioModal.js
@@ -1,17 +1,16 @@
 import React from 'react';
 
 const BioModal = ({ onClose, bio }) => {
+  const { name, paragraphs } = bio;
+
   return (
     <div className="fixed inset-0 z-50 flex items-center justify-center p-4 bg-black bg-opacity-50">
       <div className="relative w-full max-w-md p-8 bg-white rounded-lg shadow-lg">
-        <button
-          onClick={onClose}
-          className="absolute top-2 right-2 text-xl font-semibold"
-        >
+        <button onClick={onClose} className="absolute top-2 right-2 text-xl font-semibold">
           &times;
         </button>
-        <h3 className="text-2xl font-semibold mb-4">{`Bio: ${bio.name}`}</h3>
-        {bio.paragraphs.map((paragraph, index) => (
+        <h3 className="text-2xl font-semibold mb-4">{`Bio: ${name}`}</h3>
+        {paragraphs.map((paragraph, index) => (
           <p className="text-lg font-light mb-4" key={index}>
             {paragraph}
           </p>
