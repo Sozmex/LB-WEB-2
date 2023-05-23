5680f1ad3ce0e2c761e8eb1539337ee322090ccf
Sozmex <zachmex1@gmail.com>
2023-05-23 15:33:01 -0400

    responsiveness edits

--- src/components/layout.js
+++ src/components/layout.js
@@ -29,7 +29,7 @@ const Layout = ({ location, title, children }) => {
         {header}
       </header>
       <main>{children}</main>
-      <Footer>
+      <Footer >
       
       </Footer>
     </div>
